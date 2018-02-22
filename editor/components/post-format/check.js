27ffe0fa76d57cfd979e3a7e767fc12136fbcb08
Jorge <jorge.costa@developer.pt>
2018-02-22 15:08:19 +0000

    Disable post format input if the theme does not support post formats.
    This is the same behaviour we have currently on the classic. Showing the post format input also has another non-desirable side effect, when the theme does not support post formats, posts are created with standard post format and not the default post format setting. As the setting is not respected, leaving the input there confuses the user.

--- editor/components/post-format/check.js
+++ editor/components/post-format/check.js
@@ -1,10 +1,21 @@
 /**
+ * WordPress dependencies
+ */
+import { withContext } from '@wordpress/components';
+
+/**
  * Internal dependencies
  */
 import PostTypeSupportCheck from '../post-type-support-check';
 
-function PostFormatCheck( props ) {
-	return <PostTypeSupportCheck { ...props } supportKeys="post-formats" />;
+function PostFormatCheck( { themeSupportsPostFormats, ...props } ) {
+	return themeSupportsPostFormats &&
+		<PostTypeSupportCheck { ...props } supportKeys="post-formats" />;
 }
 
-export default PostFormatCheck;
+export default withContext( 'editor' )(
+	( settings ) => ( {
+		themeSupportsPostFormats: settings.themeSupportsPostFormats,
+	} )
+)( PostFormatCheck );
+
